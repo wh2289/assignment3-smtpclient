6b4590677bda8d162354b513c907c99c359dd4b3
wh2289 <93686988+wh2289@users.noreply.github.com>
2022-02-26 22:57:08 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -34,7 +34,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # if recv1[:3] != '250':
     # print('250 reply not received from server.')
 
-    data = "DATA \r\n"
+    data = "DATA\r\n"
     clientSocket.send(data.encode())
     clientSocket.send(msg.encode())
     recv4 = clientSocket.recv(1024).decode()
