e0d3dcf84bfea148ad6b3be8130f48ca397d857b
wh2289 <93686988+wh2289@users.noreply.github.com>
2022-02-26 20:29:37 -0500

    update

--- solution.py
+++ solution.py
@@ -2,20 +2,19 @@ from socket import *
 
 
 def smtp_client(port=1025, mailserver='127.0.0.1'):
-    msg = "\r\n My message"
+    msg = "\r\n Go Cyber Fellows!"
     endmsg = "\r\n.\r\n"
 
-    # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
+    mailserver = ("mail.smtp2go.com", 2525)
 
-    # Create socket called clientSocket and establish a TCP connection with mailserver and port
-
-    # Fill in start
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect(mailserver)
     # Fill in end
 
     recv = clientSocket.recv(1024).decode()
-    #print(recv) #You can use these print statement to validate return codes from the server.
-    #if recv[:3] != '220':
-    #    print('220 reply not received from server.')
+    print("Connection Requst:" + recv)
+    if recv[:3] != '220':
+        print('220 reply not received from server.')
 
     # Send HELO command and print server response.
     heloCommand = 'HELO Alice\r\n'
