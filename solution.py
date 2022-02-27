24937232dca427e2f422f5803f1d386464eca0df
wh2289 <93686988+wh2289@users.noreply.github.com>
2022-02-26 21:13:45 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,10 @@
 from socket import *
 
-
-
-def smtp_client(port=1025, mailserver ='127.0.0.1'):
     msg = "\r\n Go NYU Cyber Fellows!"
     endmsg = "\r\n.\r\n"
 
+
+def smtp_client(port=1025, mailserver ='127.0.0.1'):
 
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect(mailserver, port)
