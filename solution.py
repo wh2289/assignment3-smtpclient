d18131824122c35fba229d514ce6e507b3643f08
wh2289 <93686988+wh2289@users.noreply.github.com>
2022-02-26 20:50:05 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,12 +1,12 @@
 from socket import *
-import base64
 
 
-def smtp_client(port=1025, mailserver='127.0.0.1'):
+
+def smtp_client(port=1025, mailserver ='127.0.0.1'):
     msg = "\r\n Go NYU Cyber Fellows!"
     endmsg = "\r\n.\r\n"
 
-    mailserver = ("mail.smtp2go.com", 2525)
+    mailserver = ("local", 7890)
 
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect(mailserver)
@@ -18,8 +18,8 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
         print('220 reply not received from server.')
 
 
-    heloCommand = 'HELO Alice\r\n'
-    clientSocket.send(heloCommand.encode())
+    helloCommand = 'HELLO Alice\r\n'
+    clientSocket.send(helloCommand.encode())
     recv1 = clientSocket.recv(1024).decode()
     print(recv1)
     if recv1[:3] != '250':
@@ -32,15 +32,20 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     if recv1[:3] != '250':
         print('250 reply not received from server.')
 
+    rcptto = "Rcpt To : <destination@gmail.com> \r\n"
+    clientSocket.send(rcptto.encode())
+    recv3 = clientSocket.recv(1024)
+    print("Rcpt To: " + recv3)
+    if recv1[:3] != '250'
+        print('250 reply not received from server.')
 
+    data = "Data \r\n"
+    clientSocket.send(data.encode())
+    recv4 = clientSocket.recv(1024)
+    print("Data: " + recv4)
+    if recv1[:3] != '250'
+        print('250 reply not received from server.')
 
-    # Send RCPT TO command and handle server response.
-    # Fill in start
-    # Fill in end
-
-    # Send DATA command and handle server response.
-    # Fill in start
-    # Fill in end
 
     # Send message data.
     # Fill in start
@@ -50,9 +55,10 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Fill in start
     # Fill in end
 
-    # Send QUIT command and handle server response.
-    # Fill in start
-    # Fill in end
+    clientSocket.send("Quit\r\n".encode())
+    messsage = clientSocket.recv(1024)
+    print(message)
+    clientSocket.close()
 
 
 if __name__ == '__main__':
